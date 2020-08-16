7624fa09ea81c54bbb6c5669f32b611e034ca928
MAPLE\extri <x>
2020-08-16 13:16:56 -0400

    added noli's icon

--- KMS2Launcher/MainForm.Designer.cs
+++ KMS2Launcher/MainForm.Designer.cs
@@ -43,7 +43,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnDownloadInstall = new System.Windows.Forms.Button();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label11 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cbFontsize = new System.Windows.Forms.ComboBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnSetKRFont = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnGetKRFont = new System.Windows.Forms.Button();
             this.cbVoiceLanguage = new System.Windows.Forms.ComboBox();
             this.label9 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
@@ -58,15 +67,6 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.btnGetKRFont = new System.Windows.Forms.Button();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.btnSetKRFont = new System.Windows.Forms.Button();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.cbFontsize = new System.Windows.Forms.ComboBox();
             this.tabControl.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -238,6 +238,17 @@
             this.tabPage3.Text = "Patches and tweaks";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(14, 118);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(775, 16);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Warning: Patches can get overridden by official game patches. you may need to red" +
+    "ownload patches when a new update rolls out.";
+            // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.cbFontsize);
@@ -256,6 +267,86 @@
             this.groupBox3.TabIndex = 11;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Game tweaks";
+            // 
+            // cbFontsize
+            // 
+            this.cbFontsize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFontsize.FormattingEnabled = true;
+            this.cbFontsize.Items.AddRange(new object[] {
+            "100%",
+            "95%",
+            "90%",
+            "85%"});
+            this.cbFontsize.Location = new System.Drawing.Point(74, 230);
+            this.cbFontsize.Name = "cbFontsize";
+            this.cbFontsize.Size = new System.Drawing.Size(101, 21);
+            this.cbFontsize.TabIndex = 21;
+            this.cbFontsize.SelectedIndexChanged += new System.EventHandler(this.cbFontsize_SelectedIndexChanged);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(16, 233);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(52, 13);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "Font size:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(16, 212);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(123, 13);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Step 3: Change font size";
+            // 
+            // btnSetKRFont
+            // 
+            this.btnSetKRFont.Location = new System.Drawing.Point(19, 167);
+            this.btnSetKRFont.Name = "btnSetKRFont";
+            this.btnSetKRFont.Size = new System.Drawing.Size(255, 33);
+            this.btnSetKRFont.TabIndex = 18;
+            this.btnSetKRFont.Text = "Set \"Noto Sans RK\" font in fontconfig.txt file";
+            this.btnSetKRFont.UseVisualStyleBackColor = true;
+            this.btnSetKRFont.Click += new System.EventHandler(this.btnSetKRFont_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(16, 151);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(212, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Step 2: Change the game font and font size";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 89);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(182, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Step 1: Download and install this font";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(178, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Installing a cleaner font for the game";
+            // 
+            // btnGetKRFont
+            // 
+            this.btnGetKRFont.Location = new System.Drawing.Point(19, 105);
+            this.btnGetKRFont.Name = "btnGetKRFont";
+            this.btnGetKRFont.Size = new System.Drawing.Size(255, 33);
+            this.btnGetKRFont.TabIndex = 14;
+            this.btnGetKRFont.Text = "Get \"Noto Sans KR\" font from Google Fonts";
+            this.btnGetKRFont.UseVisualStyleBackColor = true;
+            this.btnGetKRFont.Click += new System.EventHandler(this.btnGetKRFont_Click);
             // 
             // cbVoiceLanguage
             // 
@@ -399,97 +490,6 @@
             this.tabPage4.Text = "Launch";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // btnGetKRFont
-            // 
-            this.btnGetKRFont.Location = new System.Drawing.Point(19, 105);
-            this.btnGetKRFont.Name = "btnGetKRFont";
-            this.btnGetKRFont.Size = new System.Drawing.Size(255, 33);
-            this.btnGetKRFont.TabIndex = 14;
-            this.btnGetKRFont.Text = "Get \"Noto Sans KR\" font from Google Fonts";
-            this.btnGetKRFont.UseVisualStyleBackColor = true;
-            this.btnGetKRFont.Click += new System.EventHandler(this.btnGetKRFont_Click);
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(16, 64);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(178, 13);
-            this.label7.TabIndex = 15;
-            this.label7.Text = "Installing a cleaner font for the game";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(16, 89);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(182, 13);
-            this.label8.TabIndex = 16;
-            this.label8.Text = "Step 1: Download and install this font";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(16, 151);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(212, 13);
-            this.label10.TabIndex = 17;
-            this.label10.Text = "Step 2: Change the game font and font size";
-            // 
-            // btnSetKRFont
-            // 
-            this.btnSetKRFont.Location = new System.Drawing.Point(19, 167);
-            this.btnSetKRFont.Name = "btnSetKRFont";
-            this.btnSetKRFont.Size = new System.Drawing.Size(255, 33);
-            this.btnSetKRFont.TabIndex = 18;
-            this.btnSetKRFont.Text = "Set \"Noto Sans RK\" font in fontconfig.txt file";
-            this.btnSetKRFont.UseVisualStyleBackColor = true;
-            this.btnSetKRFont.Click += new System.EventHandler(this.btnSetKRFont_Click);
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(14, 118);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(775, 16);
-            this.label11.TabIndex = 16;
-            this.label11.Text = "Warning: Patches can get overridden by official game patches. you may need to red" +
-    "ownload patches when a new update rolls out.";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(16, 212);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(123, 13);
-            this.label12.TabIndex = 19;
-            this.label12.Text = "Step 3: Change font size";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(16, 233);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(52, 13);
-            this.label13.TabIndex = 20;
-            this.label13.Text = "Font size:";
-            // 
-            // cbFontsize
-            // 
-            this.cbFontsize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbFontsize.FormattingEnabled = true;
-            this.cbFontsize.Items.AddRange(new object[] {
-            "100%",
-            "95%",
-            "90%",
-            "85%"});
-            this.cbFontsize.Location = new System.Drawing.Point(74, 230);
-            this.cbFontsize.Name = "cbFontsize";
-            this.cbFontsize.Size = new System.Drawing.Size(101, 21);
-            this.cbFontsize.TabIndex = 21;
-            this.cbFontsize.SelectedIndexChanged += new System.EventHandler(this.cbFontsize_SelectedIndexChanged);
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -497,9 +497,10 @@
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(884, 461);
             this.Controls.Add(this.tabControl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(900, 500);
             this.Name = "MainForm";
-            this.Text = "KMS2 Launcher";
+            this.Text = "kms2launcher";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.tabControl.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
