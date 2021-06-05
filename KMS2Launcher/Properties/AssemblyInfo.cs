14cfac4b602bd46d124442379940069595df586d
Phil Girard <phil@redscientist.com>
2021-06-05 19:57:58 -0400

    Changed assembly metadata to prevent weird ML detection

--- KMS2Launcher/Properties/AssemblyInfo.cs
+++ KMS2Launcher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KMS2Launcher")]
-[assembly: AssemblyCopyright("Copyright (C) 2020")]
+[assembly: AssemblyCopyright("Copyright (C) 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("109129d3-5739-4cf9-b614-4288eb9cc350")]
+[assembly: Guid("109129d3-5739-4cf9-b614-4288eb9cc355")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
