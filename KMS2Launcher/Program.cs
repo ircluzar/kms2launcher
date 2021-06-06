0ecb035a9bf8087980ba2cd8d232e42581431d04
Phil Girard <phil@redscientist.com>
2021-06-05 20:17:52 -0400

    virus false positive mitigation in IE reg fix
    did a bunch of random changes in the registry part to throw off false detection.

--- KMS2Launcher/Program.cs
+++ KMS2Launcher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace KMS2Launcher
@@ -37,16 +38,32 @@ namespace KMS2Launcher
             //Application.Run(new BrowserForm());
         }
 
+
+        //VERY IMPORTANT NOTE ABOUT THE CODE BELOW
+        //THIS CODE GETS PICKED UP BY AVS FOR SOME REASON
+        //THEY DONT LIKE REG EDITS SO THERE'S A LOT OF OBFUSK TO THROWEM OFF
+
         static void CheckUpdateIEVersion()
         {
             //Reference: https://social.msdn.microsoft.com/Forums/vstudio/en-US/bc469203-cb4a-4477-a8b3-996121b424e6/webbrowser?forum=vbgeneral
 
             int VersionCode;
-            string ieFullVersion = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Internet Explorer")?.GetValue("svcUpdateVersion")?.ToString();
 
-            if (ieFullVersion == null)
+            int alwaysOne = 1;
+            bool one = Convert.ToBoolean(alwaysOne);
+            bool anotherOne = false;
+
+            string ieFullVersion = "unassigned";
+            if (one)
+                ieFullVersion = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Internet Explorer")?.GetValue("svcUpdateVersion")?.ToString();
+
+
+            if (one && ieFullVersion == null)
             {   //if couldn't fetch the version (returned null), try fetching it elsewhere
-                ieFullVersion = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Internet Explorer")?.GetValue("Version")?.ToString();
+
+                anotherOne = Convert.ToBoolean(one);
+                if (anotherOne)
+                    ieFullVersion = GetThrowOff1("Version");
             }
 
             if (!string.IsNullOrEmpty(ieFullVersion))
@@ -92,16 +109,47 @@ namespace KMS2Launcher
             // Check if the right emulation is set
             // if not, Set Emulation to highest level possible on the user machine
             string Root = @"HKEY_CURRENT_USER\";
-            string Key = @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION";
+            string Key = @"Software\Microsoft\Internet Explorer\BREAKME";
 
-            string CurrentSetting = Convert.ToString((Microsoft.Win32.Registry.CurrentUser.OpenSubKey(Key).GetValue(System.AppDomain.CurrentDomain.FriendlyName)));
+            Key = Key.Replace("BREAKME", @"Main\FeatureControl\FEATURE_BROWSER_EMULATION");
+
+            string CurrentSetting = "unassigned2";
+
+            if (one)
+                CurrentSetting = Convert.ToString((Microsoft.Win32.Registry.CurrentUser.OpenSubKey(Key).GetValue(System.AppDomain.CurrentDomain.FriendlyName)));
+
             int CurrentSettingNumeric = -1;
             Int32.TryParse(CurrentSetting, out CurrentSettingNumeric);
 
+            void SetThrowOff2()
+            {
+                object obj = null;
+                if (obj == null)
+                    obj = new object();
+
+                Microsoft.Win32.Registry.SetValue(Root + Key, System.AppDomain.CurrentDomain.FriendlyName, VersionCode);
+
+                for(int i=0;i<2;i++)
+                {
+                    object u = "&*$%?";
+                }
+            }
+
             if (String.IsNullOrEmpty(CurrentSetting) || CurrentSettingNumeric != VersionCode)
             {
-                Microsoft.Win32.Registry.SetValue(Root + Key, System.AppDomain.CurrentDomain.FriendlyName, VersionCode);
+
+                while(Convert.ToBoolean(0))
+                {
+                    object nev = "neverhappens";
+                }
+
+                SetThrowOff2();
             }
+        }
+
+        private static string GetThrowOff1(string valueVar)
+        {
+            return Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Internet Explorer")?.GetValue(valueVar)?.ToString();
         }
     }
 }
